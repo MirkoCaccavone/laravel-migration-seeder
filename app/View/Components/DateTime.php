f3ad559c666f6549a276b0e4a7abbab5c54e5fd1
Mirko <mirkocaccavone03@gmail.com>
2025-05-20 11:27:26 +0200

    Enhance comments in DateTime component and home view for better clarity

--- app/View/Components/DateTime.php
+++ app/View/Components/DateTime.php
@@ -5,6 +5,7 @@ namespace App\View\Components;
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
+// libreria PHP per la gestione delle date e del tempo
 use Carbon\Carbon;
 
 class DateTime extends Component
@@ -17,7 +18,9 @@ class DateTime extends Component
      */
     public function __construct()
     {
+        // impostiamo la data corrente usando Carbon
         $now = Carbon::now('Europe/Rome');
+        // Formattiamo la data nel formato italiano (giorno/mese/anno)
         $this->currentDate = $now->format('d/m/Y');
         $this->currentTime = $now->format('H:i:s');
     }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    {{-- Meta tags per la configurazione della pagina --}}
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    {{-- Inclusione di Font Awesome per le icone --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    
+    {{-- Inclusione degli asset compilati con Vite (CSS e JS) --}}
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <title>laravel migration seeder</title>
 </head>
+
 <body>
+    {{-- Container principale con margine superiore --}}
     <div class="container mt-5">
+        {{-- Titolo principale centrato --}}
         <h1 class="text-center mb-4">Tabellone Partenze</h1>
 
+        {{-- Componente per la visualizzazione di data e ora --}}
         <x-date-time />
 
         <div class="table-responsive">
             <table class="table table-striped table-hover">
                 <thead class="table-dark">
                     <tr>
+                        {{-- Colonne della tabella --}}
                         <th>Azienda</th>
                         <th>Codice Treno</th>
                         <th>Data</th>
@@ -30,10 +40,12 @@
                     </tr>
                 </thead>
                 <tbody>
-                    {{-- usiamo il forelse per far si che se non ci sono treni possiamo usare il empty e visualizzare il messaggio --}}
+                    {{-- Ciclo forelse per iterare sui treni con gestione caso vuoto --}}
                     @forelse ($treni as $treno)
+                        {{-- Componente che renderizza ogni riga del treno --}}
                         <x-train-component :treno="$treno"/>
                     @empty
+                        {{-- Messaggio mostrato quando non ci sono treni --}}
                         <tr>
                             <td colspan="9" class="text-center">Nessun treno in partenza da oggi.</td>
                         </tr>
